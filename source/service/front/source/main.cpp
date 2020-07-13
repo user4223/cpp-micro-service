184a1f33ff3196ae25cee1f2a2c6d99719d4ab3b
karlheinzkurt <s.lumma@gmail.com>
2020-07-13 21:53:03 +0200

    Make hostname a program option

--- source/service/front/source/main.cpp
+++ source/service/front/source/main.cpp
@@ -8,6 +8,8 @@
 #include <memory>
 #include <string>
 
+#include <boost/program_options.hpp>
+
 static const std::map<std::string, std::string> carEngineMap =
     { ///< TODO Move out to persistence service
         {"delorean", "flux capacitor"},
@@ -17,33 +19,53 @@ static const std::map<std::string, std::string> carEngineMap =
 int
 main(int argc, char const** argv)
 {
+  namespace bpo = boost::program_options;
+
+  bpo::options_description general("General options");
+  general.add_options()("help,h", "This help message")(
+      "hostname,n", bpo::value<std::string>()->default_value("localhost"), "Hostname to listen on");
+
+  bpo::variables_map vm;
+  bpo::store(bpo::parse_command_line(argc, argv, general), vm);
+  bpo::notify(vm);
+
+  if (vm.count("help"))
+  {
+    std::cout << general << "\n";
+    return 0;
+  }
 
   crossplat::threadpool::initialize_with_threads(
       1); ///< Just avoid 40 threads created by http::listener by default
 
-  auto handler = std::make_unique<Common::CRouteRegistry>();
+  const auto handler = std::make_unique<Common::CRouteRegistry>();
   handler->add("/car/.*", {API::Method::Get},
                [](API::Method method, web::http::http_request request) {
                  const auto path = web::uri::decode(request.relative_uri().path());
-
                  std::cout << "Got request on: " << path << std::endl;
 
                  const auto parts = web::uri::split_path(path);
-                 if (parts.size() < 2) return request.reply(web::http::status_codes::NotFound);
+                 if (parts.size() < 2)
+                 {
+                   return request.reply(web::http::status_codes::NotFound);
+                 }
 
                  const auto engine = carEngineMap.find(parts[1]);
                  if (engine == carEngineMap.end())
+                 {
                    return request.reply(web::http::status_codes::NotFound);
+                 }
 
                  auto response = web::json::value::object();
                  response["engine"] = web::json::value(engine->second);
                  return request.reply(web::http::status_codes::OK, response);
                });
 
-  // TODO Validate host argument
-  const auto uri =
-      web::uri_builder("http://").set_host(argv[1]).set_port(std::to_string(6565)).set_path("v1.0");
-  auto router = std::make_unique<Common::CRouter>(uri.to_uri(), *handler);
+  const auto uri = web::uri_builder("http://")
+                       .set_host(vm["hostname"].as<std::string>())
+                       .set_port(std::to_string(6565))
+                       .set_path("v1.0");
+  const auto router = std::make_unique<Common::CRouter>(uri.to_uri(), *handler);
   std::cout << "Listening on: " << uri.to_string() << std::endl;
 
   Utility::awaitInterrupt();
